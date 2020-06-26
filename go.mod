9f447d80f132b3d2ec6246e1fd8251d563730913
junyangzhang <network.yang.0@gmail.com>
2020-06-26 20:48:45 +0800

    example

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/young-0
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,15 @@
+// __author__ = 'jean'
+// Created by zhang at 2020/6/26 20:38
+// Process by go
+
+package main
+
+import (
+	"fmt"
+	"github.com/young-0/mathy"
+)
+
+func main() {
+	fmt.Println(mathy.Fib(10))
+	fmt.Println(mathy.Fact(10))
+}
